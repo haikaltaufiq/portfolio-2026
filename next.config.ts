bad95d875d93e28469cc668255af967ad0b38fc1
HaikalTaufiq <134383454+HaikalTaufiq@users.noreply.github.com>
2026-02-20 16:26:23 +0700

    update next.config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", // Tambahin baris ini bjir
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
